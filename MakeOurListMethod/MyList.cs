473af5142017ff7f469c116f14614ca228b8d937
EnjoyKing <mertacareee@gmail.com>
2022-09-04 00:42:58 +0300

    Codes

---
+++ MakeOurListMethod/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace MakeOurListMethod
+{
+    class MyList<T>
+    {
+        T[] items; // T can be everything like int,string,etc.
+        public MyList()
+        {
+            items = new T[0];
+            //Everytime MyList<T> have been made new(),an new T array with 1 item is created.
+        }
+        public void Add(T addedItem)
+        {
+            T[] tempArray = items; //We don't wanna lose old references.
+            items = new T[items.Length + 1];//+1 because we'll add new items.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+                //Give back what's mine :)
+            }
+            items[items.Length - 1] = addedItem; //-1 because,array's starts with 0(for index)
+        }
+        public int LenghtOfArray { get {return items.Length; }  } //We don't need set.
+        public T[] ListItems { get { return items; } }
+    }
+}
---
+++ MakeOurListMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MakeOurListMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> languages = new MyList<string>();
+            languages.Add("Turkish");
+            Console.WriteLine(languages.LenghtOfArray);
+            languages.Add("English");
+            Console.WriteLine(languages.LenghtOfArray);
+            foreach (var language in languages.ListItems)
+            {
+                Console.WriteLine(language);
+            }
+        }
+    }
+}
